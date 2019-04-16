ccef31b4a1812030635159573d2f84c3c1ee33c6
zhdbest <zhd94811@163.com>
2019-04-16 20:49:03 +0800

    2019-04-16：TreeMap相关

--- src/main/java/com/basic/map/treemap/TreeMapDemo.java
+++ src/main/java/com/basic/map/treemap/TreeMapDemo.java
@@ -1,5 +1,6 @@
 package com.basic.map.treemap;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -10,9 +11,25 @@ import java.util.TreeMap;
 public class TreeMapDemo {
 
     public static void main(String[] args) {
-        Map<String, Object> map = new TreeMap<String, Object>();
-        map.put(null, null);
-        map.put("1", "一");
+        Map<Long, Object> defaultTreeMap = getDefaultTreeMap();
+//        defaultTreeMap.put(null ,null);
 
+        // 指定了比较器的TreeMap是可能支持key为null的
+        Map<Long, Object> diyTreeMap = getDIYTreeMap();
+        diyTreeMap.put(null, null);
+        System.out.println(diyTreeMap);
+        diyTreeMap.remove(null);
+    }
+
+    private static TreeMap<Long, Object> getDefaultTreeMap() {
+        return new TreeMap<Long, Object>();
+    }
+
+    private static TreeMap<Long, Object> getDIYTreeMap() {
+        return new TreeMap<Long, Object>(new Comparator<Long>() {
+            public int compare(Long o1, Long o2) {
+                return 0;
+            }
+        });
     }
 }
