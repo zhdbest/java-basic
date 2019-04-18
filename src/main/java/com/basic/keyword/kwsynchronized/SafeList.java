f341a91fa3d4fe1a4a24e2ccfd6f6d094bd4faea
zhdbest <zhd94811@163.com>
2019-04-18 22:58:12 +0800

    2019-04-18：synchronized关键字demo

---
+++ src/main/java/com/basic/keyword/kwsynchronized/SafeList.java
@@ -0,0 +1,30 @@
+package com.basic.keyword.kwsynchronized;
+
+/**
+ * 一个极其简易的List，只可用于验证并发
+ * @author haodong.zhao
+ * @version 1.0. Added Time:2019/4/18 20:56
+ */
+public class SafeList<T> {
+
+    private Object[] table;
+
+    private int size;
+
+    public SafeList(int initCapacity) {
+        table = new Object[initCapacity];
+    }
+
+    public synchronized boolean add(T t) {
+        table[size++] = t;
+        return true;
+    }
+
+    public Object get(int index) {
+        return table[index];
+    }
+
+    public int size() {
+        return size;
+    }
+}
