593d909bf7a246954e77f8d4de459ab14cdf823b
haodong.zhao <haodong.zhao@china.zhaogang.com>
2019-05-16 20:19:49 +0800

    反射demo

---
+++ src/main/java/com/basic/lang/reflect/person/model/Student.java
@@ -0,0 +1,99 @@
+package com.basic.lang.reflect.person.model;
+
+/**
+ * @author haodong.zhao
+ * @version 1.0. Added Time:2019/4/22 14:55
+ */
+public class Student {
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private Integer age;
+
+    /**
+     * 家庭地址
+     */
+    private String address;
+
+    /**
+     * 新增人
+     */
+    private Long addedBy;
+
+    /**
+     * 新增人名称
+     */
+    private String addedByName;
+
+    /**
+     * 最后修改人
+     */
+    private Long lastModifiedBy;
+
+    /**
+     * 最后修改人名称
+     */
+    private String lastModifiedByName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(Long addedBy) {
+        this.addedBy = addedBy;
+    }
+
+    public Long getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(Long lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public String getAddedByName() {
+        return addedByName;
+    }
+
+    public void setAddedByName(String addedByName) {
+        this.addedByName = addedByName;
+    }
+
+    public String getLastModifiedByName() {
+        return lastModifiedByName;
+    }
+
+    public void setLastModifiedByName(String lastModifiedByName) {
+        this.lastModifiedByName = lastModifiedByName;
+    }
+}
